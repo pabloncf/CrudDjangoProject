5095d647ce68d3c0ed88a48955f0c70e295ae836
Pablo <pabloncf@gmail.com>
2023-04-01 22:40:59 -0300

    commit atualização pages

--- locadora_carros/views.py
+++ locadora_carros/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Carro,Cliente
 from .serializer import CarroSerializer,ClienteSerializer
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 class CarroViewSet(viewsets.ModelViewSet):
-    queryset = Carro.objects.all()
+    queryset = Carro.objects.all().order_by('id')
     serializer_class = CarroSerializer
+    carro_paginator = Paginator(queryset,20)
 
 class ClienteViewSet(viewsets.ModelViewSet):
-    queryset = Cliente.objects.all()
+    queryset = Cliente.objects.all().order_by('nome')
     serializer_class = ClienteSerializer
+    cliente_paginator = Paginator(queryset,20)
